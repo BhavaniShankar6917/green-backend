9c6df59634939b6d382d5ccab321d42a39afcc4e
BhavaniShankar6917 <bhavanishankar6917@gmail.com>
2024-02-08 06:57:28 +0530

    Adding new controllers

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
 import { SupabaseController } from './supabase.controller';
+import { PostsController } from './posts/posts.controller';
+import { CommentsController } from './comments/comments.controller';
+import { LikesController } from './likes/likes.controller';
+import { PeopleController } from './people/people.controller';
 
 @Module({
   providers: [SupabaseService],
-  controllers: [SupabaseController]
+  controllers: [SupabaseController, PostsController, CommentsController, LikesController, PeopleController]
 })
 export class SupabaseModule {}
